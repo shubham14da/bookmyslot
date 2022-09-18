e15ddf951114b503b1ceabc39de17cc5559a7988
Divyaraj-Mandale <111412336+Divyaraj-Mandale@users.noreply.github.com>
2022-09-18 14:48:29 +0530

    Update UserController.java

--- authentication-service/src/main/java/com/stackroute/authenticationservice/controller/UserController.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
-// @CrossOrigin
+@CrossOrigin
 public class UserController {
     private ResponseEntity responseEntity;
     private UserService userService;

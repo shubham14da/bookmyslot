b167835721f43f7ee0330db8c1edf0dae430023a
Divyaraj-Mandale <111412336+Divyaraj-Mandale@users.noreply.github.com>
2022-09-18 13:56:56 +0530

    Update AuthenticationServiceApplication.java

--- authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/AuthenticationServiceApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
